dc94fd414edc54a6b1387f1501354e3d513e21d9
Anna Wu <shinshinwu@gmail.com>
2015-03-07 10:10:59 -0800

    added pw to user migration

--- db/migrate/20150307172240_create_users.rb
+++ db/migrate/20150307172240_create_users.rb
@@ -4,6 +4,7 @@ class CreateUsers < ActiveRecord::Migration
       t.string :first_name
       t.string :last_name
       t.string :email
+      t.string :password_digest
       t.string :account_name
       t.integer :account_balance
       t.integer :account_number
