b14689e56ed7bb35d927f1a7db790ec451b5d6bf
Nick Perez <nickperez1285@gmail.com>
2015-03-08 15:34:15 -0700

    fixed anna s number

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -78,7 +78,7 @@ nexmo = Nexmo::Client.new(key:'ec19c1ba', secret: 'ba674a8a')
 
 response = nexmo.send_message({
   from: '12529178633',
-  to: '4014972654',
+  to: '14014972654',
   text: 'Transaction completed'
 })
 
